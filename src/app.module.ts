960bcec65a95644c7e3195e81601ac1a1aee4324
JazimAbbas <stackdeveloper87@gmail.com>
2021-01-21 14:02:10 +0500

    added mysql database connection with our app ..

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmOptions } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
