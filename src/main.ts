a0461c2158836ee2a7727dc8f14ce0f6bd171c79
JazimAbbas <stackdeveloper87@gmail.com>
2021-01-25 10:37:41 +0500

    serve static files as public so everyone can see this ..

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
 
 import { AppModule } from './app.module';
+import { UPLOAD_DIR } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(`/${UPLOAD_DIR}`, express.static(UPLOAD_DIR));
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
