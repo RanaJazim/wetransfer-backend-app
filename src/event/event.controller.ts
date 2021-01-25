d7e84ad2860cf47e086838fa59d8b41126354b2e
JazimAbbas <stackdeveloper87@gmail.com>
2021-01-25 18:04:38 +0500

    fetch all events and single event by id ..

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 
 import { EventDto, EventFormDto } from './dtos';
 import { EventService } from './event.service';
@@ -6,6 +13,16 @@ import { EventService } from './event.service';
 @Controller('event')
 export class EventController {
   constructor(private eventService: EventService) {}
+
+  @Get()
+  async fetchAllEvents(): Promise<EventDto[]> {
+    return this.eventService.fetchAllEvents();
+  }
+
+  @Get(':id')
+  async fetchSingleEvent(@Param('id') id: number): Promise<EventDto> {
+    return this.eventService.fetchSingleEvent(id);
+  }
 
   @Post()
   async createEvent(
--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { EventDto, EventFormDto } from './dtos';
@@ -10,6 +10,16 @@ export class EventService {
     @InjectRepository(EventRepository)
     private eventRepository: EventRepository,
   ) {}
+
+  fetchAllEvents(): Promise<EventDto[]> {
+    return this.eventRepository.find();
+  }
+
+  async fetchSingleEvent(id: number): Promise<EventDto> {
+    const eventInDb = await this.eventRepository.findOne(id);
+    if (!eventInDb) throw new NotFoundException(`Event with ${id} not found`);
+    return eventInDb;
+  }
 
   createEvent(event: EventFormDto): Promise<EventDto> {
     return this.eventRepository.createEvent(event);
