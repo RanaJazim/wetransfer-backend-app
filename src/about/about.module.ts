7e66081a878b004c9bc5745801f080afc1e2d17c
JazimAbbas <stackdeveloper87@gmail.com>
2021-02-03 15:27:37 +0500

    protecting about routes to only authenticated user ..

--- src/about/about.module.ts
+++ src/about/about.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AboutController } from './about.controller';
 import { AboutRepository } from './about.repository';
 import { AboutService } from './about.service';
+import { AuthMiddleware } from 'src/middlewares/auth.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AboutRepository])],
   controllers: [AboutController],
   providers: [AboutService],
 })
-export class AboutModule {}
+export class AboutModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(AboutController);
+  }
+}
