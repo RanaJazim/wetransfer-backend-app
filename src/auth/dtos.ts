cbc13919ae17214363827c5c90a09a586b15f178
JazimAbbas <stackdeveloper87@gmail.com>
2021-02-03 14:28:04 +0500

    added auth dtos ..

---
+++ src/auth/dtos.ts
@@ -0,0 +1,28 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class UserCreateDto {
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
+
+export class UserLoginDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
+
+export class UserDto {
+  id: number;
+  username: string;
+  email: string;
+}
