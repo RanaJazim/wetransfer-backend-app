7a24cbe6b8bb9c227ff43f2ac974a7136d390406
JazimAbbas <stackdeveloper87@gmail.com>
2021-02-03 15:38:24 +0500

    protecting event routes to login users ...

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -41,11 +41,6 @@ export class EventController {
     return this.eventService.pastEvents();
   }
 
-  @Get('/sample')
-  async sample() {
-    return 'sample';
-  }
-
   @Get(':id')
   async fetchSingleEvent(@Param('id') id: number): Promise<EventDto> {
     return this.eventService.fetchSingleEvent(id);
--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EventController } from './event.controller';
 import { EventRepository } from './event.repository';
 import { EventService } from './event.service';
+import { AuthMiddleware } from 'src/middlewares/auth.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EventRepository])],
   controllers: [EventController],
   providers: [EventService],
 })
-export class EventModule {}
+export class EventModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude('/event/future', '/event/past', '/event/:id', '/event/current')
+      .forRoutes(EventController);
+  }
+}
