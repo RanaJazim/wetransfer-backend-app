862f8176178a6574face31a5b88a9ffe23ad6e1e
JazimAbbas <stackdeveloper87@gmail.com>
2021-02-03 14:29:10 +0500

    added auth repository ...

---
+++ src/auth/auth.repository.ts
@@ -0,0 +1,38 @@
+import { BadRequestException, ConflictException } from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+
+import { Auth } from './auth.entity';
+import { UserCreateDto, UserLoginDto } from './dtos';
+
+@EntityRepository(Auth)
+export class AuthRepository extends Repository<Auth> {
+  async createUser(uesrCredentials: UserCreateDto): Promise<void> {
+    const { username, email, password } = uesrCredentials;
+
+    const user = new Auth();
+    user.username = username;
+    user.email = email;
+    await user.hashPassword(password);
+
+    try {
+      await user.save();
+    } catch (err) {
+      if (err.code == 'ER_DUP_ENTRY') {
+        throw new ConflictException('Email is already exist');
+      }
+
+      throw err;
+    }
+  }
+
+  async login(user: UserLoginDto): Promise<string> {
+    const { email, password } = user;
+
+    const userInDb = await this.findOne({ where: { email } });
+
+    if (!userInDb || !(await userInDb.isPasswordMatch(password)))
+      throw new BadRequestException("Credentials doen't match");
+
+    return userInDb.generateToken();
+  }
+}
